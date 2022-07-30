bc2194941e0cf3c2633b0b9c358ff6b5984235cd
Romain Bacque <bacqueromain@orange.fr>
2022-07-31 00:18:05 +0200

    last changes

---
+++ src/app.js
@@ -0,0 +1,163 @@
+const albumModule = require('./album');
+const giftModule = require('./gift');
+const headerModule = require('./header');
+const placesModule = require('./places');
+const sheltersModule = require('./shelters');
+const utilsModule = require('./utils');
+
+const appModule = {
+  loaderDOM: document.getElementById('loader__dom'),
+  init: function() {
+    appModule.handlePageLoader(1);
+    appModule.initTheme();
+    albumModule.initAlbum();
+    appModule.initIntersectionObserver();
+    appModule.initEmailjs();
+    appModule.addEventActions();
+  },
+  handlePageLoader: function(opacity) {
+      if(opacity <= 0) {
+        headerModule.handleHeaderScroll();
+        appModule.loaderDOM.style.display = "none";  
+      } else {
+        appModule.loaderDOM.style.opacity = opacity;
+        window.setTimeout(() => {
+          appModule.handlePageLoader(opacity - 0.05);
+        }, 50);
+      }
+  },
+  initTheme: function() {
+    const darkTheme = "dark-theme",
+      iconTheme = "ri-sun-line",
+      selectedTheme = localStorage.getItem("selected-theme"),
+      selectedIcon = localStorage.getItem("selected-icon");
+
+    if (selectedTheme) {
+      document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+        darkTheme
+      );
+      themeButton.classList[selectedIcon === "ri-moon-line" ? "add" : "remove"](
+        iconTheme
+      );
+    }
+  },
+  initIntersectionObserver: function() {
+    const options = {
+      root: null,
+      rootMargin: "-10% 0px",
+      treshold: 0,
+    };
+    
+    const handleIntersect = (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.remove("reveal");
+        } else entry.target.classList.add("reveal");
+      });
+    };
+    
+    const observer = new IntersectionObserver(handleIntersect, options);
+    
+    document.querySelectorAll(".reveal, .reveal-box").forEach(reveal => {
+      observer.observe(reveal);
+    });
+  },
+  initEmailjs: function() {
+      emailjs.init(utilsModule.emailjs_user);
+  },
+  addEventActions: function() {
+    window.addEventListener("scroll", _ => {
+      appModule.handleSomeElementsDisplay;
+      appModule.handleScrollUpButton();
+      appModule.handleActiveNavLink();
+      headerModule.handleHeaderScroll();
+    });
+
+    const navToggle = document.getElementById("nav-toggle");
+    navToggle.addEventListener("click", _ => document.getElementById("nav-menu").classList.add("show-menu"));  
+
+    const navClose = document.getElementById("nav-close");
+    navClose.addEventListener("click", _ => document.getElementById("nav-menu").classList.remove("show-menu"));
+
+    const navLink = document.querySelectorAll(".nav__link");
+    navLink.forEach(link => link.addEventListener("click", headerModule.handleLinkClick));
+
+    const themeButton = document.getElementById("theme-button");
+    themeButton.addEventListener("click", headerModule.handleTheme);
+
+    const marker = document.getElementById("marker");
+    marker.addEventListener("click", placesModule.handleDescriptionDisplay.bind(null, 0));
+
+    const lieuxBanniere = document.querySelector(".lieux__bannière");
+    lieuxBanniere.addEventListener("click", placesModule.handleDescriptionDisplay.bind(null, 1));       
+
+    const panelTitles = document.querySelectorAll(".panneau__text");
+    panelTitles[0].addEventListener("click", sheltersModule.handlePicturesDisplay.bind(null, 0));
+    panelTitles[1].addEventListener("click", sheltersModule.handlePicturesDisplay.bind(null, 1));
+
+    const shelterContainer = document.querySelector(".gîtes__container");
+    shelterContainer.addEventListener("click", sheltersModule.handlePicturesDisplay.bind(null, 2));
+
+    const formButton = document.querySelectorAll(".form__button");
+    formButton[0].addEventListener("click", giftModule.handleDisplayForm.bind(null, 0));
+    formButton[1].addEventListener("click", giftModule.handleDisplayForm.bind(null, 1));
+
+    const form = document.getElementById("form");
+    form.addEventListener("submit", giftModule.handleFormSubmit);
+
+    const inputs = document.querySelectorAll("input, textarea");
+    inputs.forEach(input => input.addEventListener("input", _ => giftModule.handleInputChange(input)));
+
+    const videoButton = document.getElementById("video-button");
+    videoButton.addEventListener("click", albumModule.handleVideo);
+
+    const videoFile = document.getElementById("video-file");
+    videoFile.addEventListener("ended", albumModule.handleVideoEnding); 
+  },
+  handleScrollUpButton: function() {
+    const scrollUp = document.getElementById("scroll-up");
+    
+    if (window.scrollY >= 560) scrollUp.classList.add("show-scroll");
+    else scrollUp.classList.remove("show-scroll");
+  },
+  handleActiveNavLink: function() {
+    const sections = document.querySelectorAll("section[id]"),
+        scrollY = window.pageYOffset;
+
+    sections.forEach((section) => {
+      const sectionHeight = section.offsetHeight;
+      const sectionTop = section.offsetTop - 50;
+
+      sectionId = section.getAttribute("id");
+
+      if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+        document
+          .querySelector(`.nav__menu a[href*="${sectionId}"]`)
+          .classList.add("active-link");
+      } else {
+        document
+          .querySelector(`.nav__menu a[href*="${sectionId}"]`)
+          .classList.remove("active-link");
+      }
+    });
+  },
+  handleSomeElementsDisplay: function() {
+    const shelterAndGifSections = document.querySelectorAll("#cadeau, #gîtes");
+
+    if (
+      window.scrollY < shelterAndGifSections[1].offsetTop - 600 ||
+      window.scrollY > shelterAndGifSections[1].offsetHeight + shelterAndGifSections[1].offsetTop
+    ) {
+      sheltersModule.hideShelters();
+    }
+    if (
+      window.scrollY < shelterAndGifSections[0].offsetTop - 600 ||
+      window.scrollY > shelterAndGifSections[0].offsetHeight + shelterAndGifSections[0].offsetTop
+    ) {
+      giftModule.hideForm();
+    }
+  }
+}
+
+window.addEventListener("load", appModule.init);
+
---
+++ src/gift.js
@@ -0,0 +1,162 @@
+
+const utilsModule = require('./utils');
+
+const giftModule = {
+    formContainer: document.getElementById("container__form"),
+    firstName: document.getElementById("user__firstname"),
+    lastName: document.getElementById("user__lastname"),
+    email: document.getElementById("user__email"),
+    tel: document.getElementById("user__tel"),
+    msg: document.getElementById("msg"),
+    loader: document.getElementById("loader"),
+    inputs: document.querySelectorAll("input, textarea"),
+    handleFormSubmit: function(event) {
+        event.preventDefault();
+
+        giftModule.checkInputs();
+    },
+    handleDisplayForm: function(value) {
+        if (value === 0) {
+            giftModule.hideForm();
+        } else if (value === 1) {
+            giftModule.showForm();
+        }
+    },
+    handleInputChange: function(input) {
+        if (!input.value) input.parentElement.className = "form__control";
+    },
+    handleMinAndMaxDate: function(event) { 
+        const form = document.getElementById("form");
+
+        event.target.setAttribute("min", giftModule.getLimitDate(0));
+        event.target.setAttribute("max", giftModule.getLimitDate(2));
+        form.elements.todate.setAttribute("min", form.elements.fromdate.value);
+        form.elements.todate.value < form.elements.fromdate.value
+            ? (form.elements.todate.value = form.elements.fromdate.value)
+            : "";
+    },
+    getLimitDate: function(value) {
+        const today = new Date();
+        const day = today.getDate();
+        const month = today.getMonth() + 1;
+        const year = today.getFullYear() + value;
+    
+        if (day < 10) {
+            day = "0" + day;
+        }
+        if (month < 10) {
+            month = "0" + month;
+        }
+    
+        return year + "-" + month + "-" + day;
+    },  
+    hideForm: function(){
+        giftModule.clearInputsText();
+        giftModule.formContainer.style.display = "none";
+        giftModule.formContainer.classList.remove("container__form");
+    },
+    showForm: function(){
+        const dates = document.querySelectorAll(".timepicker");
+        
+        giftModule.formContainer.style.display = "flex";
+        giftModule.formContainer.classList.add("container__form");
+
+        for (date of dates) {
+            date.addEventListener("click", giftModule.handleMinAndMaxDate);   
+            date.addEventListener("input", giftModule.handleMinAndMaxDate);
+        }
+    },
+    checkInputs: function() {
+        const firstNameValue = giftModule.firstName.value.trim(),
+            lastNameValue = giftModule.lastName.value.trim(),
+            emailValue = giftModule.email.value.trim(),
+            telValue = giftModule.tel.value.trim(),
+            emptyField = "Le champs ne doit pas être vide";
+    
+        if (!firstNameValue) {
+            giftModule.setErrorFor(giftModule.firstName, emptyField);
+        } else {
+            giftModule.setSuccessFor(giftModule.firstName);
+        }
+    
+        if (!lastNameValue) {
+            giftModule.setErrorFor(giftModule.lastName, emptyField);
+        } else {
+            giftModule.setSuccessFor(giftModule.lastName);
+        }
+    
+        if (!emailValue) {
+            giftModule.setErrorFor(giftModule.email, emptyField);
+        } else {
+            giftModule.setSuccessFor(giftModule.email);
+        }
+    
+        if (!telValue) {
+            giftModule.setErrorFor(giftModule.tel, emptyField);
+        } else {
+            giftModule.setSuccessFor(giftModule.tel);
+        }
+    },  
+    setErrorFor: function(input, message) {
+        const formControl = input.parentElement;
+        const small = formControl.querySelector("small");
+    
+        small.innerText = message;
+        formControl.className = "form__control error";
+    },
+    setSuccessFor: function(input) {
+        const formControl = input.parentElement;
+    
+        formControl.className = "form__control success";
+        giftModule.sendEmail();
+    },
+    sendEmail: function() {
+        giftModule.loader.style.display = "flex";
+        
+        emailjs
+        .send(utilsModule.emailjs_service, utilsModule.emailjs_template, {
+            firstName: giftModule.firstName.value,
+            lastName: giftModule.lastName.value,
+            email: giftModule.email.value,
+            tel: giftModule.tel.value,
+            msg: giftModule.msg.value != "" ? msg.value : "Aucune information complémentaire.",
+        })
+        .then(
+            response => {
+                giftModule.loader.style.display = "none";
+                giftModule.makeSuccessModal();
+                giftModule.clearInputsText();
+
+                console.log("SUCCESS!", response.status, response.text);
+            },
+            error => {
+                giftModule.loader.style.display = "none";
+                giftModule.makeErrorModal();
+
+                console.log("FAILED...", error);
+            }
+        );
+    },
+    makeSuccessModal: function() {
+        swal({
+            title: "Demande Envoyée avec succès !",
+            text: "Selon mes disponibilités, je la confirmerai par mail.",
+            icon: "success",
+            button: "OK",
+        });
+    },  
+    makeErrorModal: function() {
+        swal({
+            title: "Oups...",
+            text: "Echec de l'envoi de la demande",
+            icon: "error",
+            button: "OK",
+        });
+    },  
+    clearInputsText: function() {
+        giftModule.inputs.forEach((input) => {
+            input.value = "";
+            input.parentElement.className = "form__control";
+        });
+    },
+}
