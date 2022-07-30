dd184056f9b1bf3b2c14958a7a34fb74a702f307
Romain Bacque <bacqueromain@orange.fr>
2022-07-30 18:29:58 +0200

    last changes

--- assets/js/shelters.js
+++ assets/js/shelters.js
@@ -5,11 +5,11 @@ const sheltersModule = {
 
         switch (value) {
             case 0:
-                sheltersModule.toogleShelter1();
+                sheltersModule.toggleShelter1();
             break;
         
             case 1:
-                sheltersModule.toogleShelter2();
+                sheltersModule.toggleShelter2();
             break;
         
             case 2:
@@ -20,11 +20,11 @@ const sheltersModule = {
                 console.log(`Sorry, we are out of ${value}.`);
         }
     },
-    toogleShelter1: function() {
+    toggleShelter1: function() {
         sheltersModule.newClass[1].classList.toggle("gîte2");
         sheltersModule.newClass[0].classList.remove("gîte1");
     },
-    toogleShelter2: function() {
+    toggleShelter2: function() {
         sheltersModule.newClass[0].classList.toggle("gîte1");
         sheltersModule.newClass[1].classList.remove("gîte2");
     },
